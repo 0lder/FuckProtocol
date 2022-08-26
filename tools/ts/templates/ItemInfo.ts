32a3c22bac0c8f8f965b774863845c7fbd59c129
leon <460677869@qq.com>
2022-08-26 08:27:57 +0800

    feat:完成结构体encode ，decode

---
+++ tools/ts/templates/ItemInfo.ts
@@ -0,0 +1,17 @@
+import { FPBuffer } from "./FPBuffer";
+
+export class ItemInfo extends FPBuffer{
+    
+    public id:number = 0;
+
+    public count:number = 0;
+
+
+    public encode():void{
+
+    }
+
+    public decode(buffers:number[]):void{
+
+    }
+}
---
+++ tools/ts/templates/PlayeData.ts
@@ -0,0 +1,38 @@
+import { BufferUtills } from "./BufferUtils";
+import { EnumBufferType } from "./EnumBufferType";
+import { FPBuffer } from "./FPBuffer";
+import { ItemInfo } from "./ItemInfo";
+
+export class PlayerData extends FPBuffer{
+
+    public id:number = 0;
+
+    public name:string = "";
+
+    public isNew:boolean = false;
+
+    public items:ItemInfo[] = [];
+
+    public encode():number[]{
+
+        this._buffers =  [];
+        this.writeInt(this.id,EnumBufferType.INT32);
+        this.writeString(this.name);
+        this.writeBoolean(this.isNew);
+
+        return this._buffers;
+    }
+
+    public decode(buffs:number[]){
+        this._buffers = buffs;
+        this.id = this.readInt();
+        this.name = this.readString();
+        this.isNew = this.readBoolean();
+        const arrLen:number = this.readInt();
+        for (let index = 0; index < arrLen; index++) {
+            const item:ItemInfo = new ItemInfo();
+            item.decode(this._buffers);
+            this.items.push(item);
+        }
+    }
+}
