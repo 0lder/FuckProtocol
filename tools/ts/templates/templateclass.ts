9833986423a971cc7a2eadd48f4d8f649c442b06
leon <460677869@qq.com>
2022-08-26 09:26:21 +0800

    feat:add templateclass

---
+++ tools/ts/templates/templateclass.ts
@@ -0,0 +1,21 @@
+import { FPBuffer } from "./FPBuffer";
+import { EnumBufferType } from "./EnumBufferType";
+
+//val4
+
+export class templateclass extends FPBuffer {
+
+//val1
+
+    public encode(): number[] {
+
+//val2
+        return this._buffers;
+    }
+
+    public decode(): void {
+
+//val3
+
+    }
+}
